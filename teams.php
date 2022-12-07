dd3ed4e85366ac977dbce675d14730ece2673097
RadiaJannat <101438221+RadiaJannat@users.noreply.github.com>
2022-12-06 19:43:27 -0700

    Update teams.php

--- teams.php
+++ teams.php
@@ -1,43 +1,64 @@
 <?php 
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 if(isset($_SESSION['Email'])){
-    ?>
 
-    <!DOCTYPE html>
-    <html>
-
-    <head>
-    <title>Teams</title>
-    </head>
-
-    <body>
-        <h1>This is the Teams Page</h1>
-        <p>Here you will access your teams</p>
-        <?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Teams</title>
+</head>
+<style>
+    .button{
+        width:30px;
+    }
+</style>
+<body>
+    <h1>This is the Teams Page</h1>
+    <p>Here you will access your teams</p>
+    <?php
         include 'db_connection.php';
-        $user_query = $conn->prepare('SELECT * FROM TEAM WHERE TEAM.OrgID = ?');
-        $user_query->bind_param('s',$_SESSION['OrgID']);
+
+        $isAdmin_Query->execute();
+        if($row = mysqli_fetch_assoc($isAdmin_Query->get_result())){
+            ?>
+            <p><strong>Create a new team</strong></p>
+            <form action="./new_team.php" id='team_creation' method='post'>
+                <button type="submit" name="Submit" value='Submit'>Create a new Team</button>
+            </form>
+            <?php
+        }
+        ?><p><strong>My Teams</strong></p><?php
+        $query = 'SELECT TEAM.ID, TEAM.TName FROM TEAM JOIN BELONGS ON Team.ID = BELONGS.Team_ID 
+        WHERE BELONGS.MEmail = ?';
+        $user_query = $conn->prepare($query);
+        $user_query->bind_param('s',$_SESSION['Email']);
         $user_query->execute();
         $result = $user_query->get_result();
+        if(mysqli_num_rows($result) == 0){
+            ?>
+            <html>
+                <p style="font-family:helvetica;text-align:center">You currently have no projects</p>
+            </html>
+        <?php
+        }
         while($row = mysqli_fetch_assoc($result)){
             ?>
             <form action="./team.php" id='team_submission' method='post'>
-                <input type="hidden" name="TeamID" value='<?php echo $row['ID']?>'/>
+                <input type="hidden" name="teamID" value='<?php echo $row['ID']?>'/>
                 <button type="submit" name="Submit" value='Submit'><?php echo $row['TName']?></button>
             </form>
             <?php
         }
     ?>
     <a href="./homepage.php">homepage</a><br>
-    </body>
-    </html>
-<?php
+</body>
+</html>
+<?php 
 }
 else{
     session_unset();
-    header('Location: ./index.php');
+    header('Location: ./index.php?');
     exit();
 }
 ?>
